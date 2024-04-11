ce2b65e06748a3db5809f43fd8c131955c591760
Paul Garner <paul@garnernet.com>
2024-04-11 16:08:23 +0100

    Ensure cart is using the defined interface rather than struct

--- main.go
+++ main.go
@@ -6,9 +6,20 @@ import (
 )
 
 func main() {
-	testCart := checkout.SimpleCheckout{}
+	var testCart checkout.Totaliser = &checkout.SimpleCheckout{}
+	TestCart(testCart)
 	testCart.Scan("A")
 	testCart.Scan("B")
 	i, _ := testCart.GetTotalPrice()
 	fmt.Println("Total", i)
+}
+
+func TestCart(a checkout.Totaliser) {
+	a.Clear()
+	a.Scan("A")
+	a.Scan("A")
+	a.Scan("A")
+	i, _ := a.GetTotalPrice()
+	fmt.Println("Total", i)
+	a.Clear()
 }

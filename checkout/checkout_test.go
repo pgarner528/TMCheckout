3068c7d231a6c59842d3a3121c8cad4141e8bc77
Paul Garner <paul@garnernet.com>
2024-04-11 21:12:16 +0100

    Remove the simple pricing types Tidy the testing checks up to remove the deleted pricing types

--- checkout/checkout_test.go
+++ checkout/checkout_test.go
@@ -5,36 +5,6 @@ import (
 	"testing"
 )
 
-func TestAddItem(t *testing.T) {
-	var testCart Totaliser = &SimpleCheckout{Pricing: pricing.NominalPricer{}}
-	testCart.Scan("A")
-	i, _ := testCart.GetTotalPrice()
-	if i != 1 {
-		t.Errorf("Incorrect value in cart -should be 1")
-	}
-}
-
-func TestAddItems(t *testing.T) {
-	var testCart Totaliser = &SimpleCheckout{Pricing: pricing.NominalPricer{}}
-	testCart.Scan("A")
-	testCart.Scan("B")
-	i, _ := testCart.GetTotalPrice()
-	if i != 2 {
-		t.Errorf("Incorrect value in cart -should be 2")
-	}
-}
-
-func TestAddItemWithClear(t *testing.T) {
-	var testCart Totaliser = &SimpleCheckout{Pricing: pricing.NominalPricer{}}
-	testCart.Scan("A")
-	testCart.Clear()
-	testCart.Scan("B")
-	i, _ := testCart.GetTotalPrice()
-	if i != 1 {
-		t.Errorf("Incorrect value in cart -should be 1")
-	}
-}
-
 func TestNoPricing(t *testing.T) {
 	var testCart Totaliser = &SimpleCheckout{}
 	testCart.Scan("A")
@@ -47,46 +17,10 @@ func TestNoPricing(t *testing.T) {
 	}
 }
 
-func TestSimplePricing(t *testing.T) {
-	var testCart Totaliser = &SimpleCheckout{Pricing: pricing.SimplePricer{}}
-	err := testCart.Scan("A")
-	if err != nil {
-		t.Errorf("Error returned on scan %s", err.Error())
-	}
-	i, err := testCart.GetTotalPrice()
-	if err != nil {
-		t.Errorf("Error returned on GetTotalPrice %s", err.Error())
-	}
-	if i != 50 {
-		t.Errorf("Incorrect value in cart -should be 50, actual %d", i)
-	}
-	err = testCart.Scan("B")
-	if err != nil {
-		t.Errorf("Error returned on scan %s", err.Error())
-	}
-	i, err = testCart.GetTotalPrice()
-	if err != nil {
-		t.Errorf("Error returned %s", err.Error())
-	}
-	if i != 80 {
-		t.Errorf("Incorrect value in cart -should be 80, actual %d", i)
-	}
-	err = testCart.Scan("Q")
-	if err != nil {
-		t.Errorf("Error returned on scan %s", err.Error())
-	}
-	i, err = testCart.GetTotalPrice()
-	if err == nil {
-		t.Errorf("Error NOT returned with invalid item Q in basket")
-	}
-	if i != 0 {
-		t.Errorf("Incorrect value in cart -should be 0, actual %d", i)
-	}
-}
-
 type AddItem struct {
-	item         string
-	runningTotal int
+	item          string
+	runningTotal  int
+	errorExpected bool
 }
 
 func TestMultibuyPricing(t *testing.T) {
@@ -100,6 +34,7 @@ func TestMultibuyPricing(t *testing.T) {
 		{item: "A", runningTotal: 205},
 		{item: "C", runningTotal: 225},
 		{item: "D", runningTotal: 240},
+		{item: "Z", runningTotal: 0, errorExpected: true},
 	}
 	for _, itemPrice := range testSlice {
 		err := testCart.Scan(itemPrice.item)
@@ -107,8 +42,12 @@ func TestMultibuyPricing(t *testing.T) {
 			t.Errorf("Error returned on scan %s", err.Error())
 		}
 		i, err := testCart.GetTotalPrice()
-		if err != nil {
+		if (err != nil) && (!itemPrice.errorExpected) {
 			t.Errorf("Error returned on GetTotalPrice %s", err.Error())
+		} else {
+			if (err == nil) && (itemPrice.errorExpected) {
+				t.Errorf("Error NOT returned on GetTotalPrice")
+			}
 		}
 		if i != itemPrice.runningTotal {
 			t.Errorf("Incorrect value in cart -should be %d, actual %d", itemPrice.runningTotal, i)
--- pricing/pricing.go
+++ pricing/pricing.go
@@ -51,28 +51,3 @@ func multiBreak(numberOff, multipackBreak int) (int, int) {
 	}
 	return numberOff % multipackBreak, numberOff / multipackBreak
 }
-
-type SimplePricer struct {
-}
-
-func (N SimplePricer) ItemPrice(item string, numberOff int) (int, error) {
-	switch {
-	case strings.EqualFold("A", item):
-		return 50 * numberOff, nil
-	case strings.EqualFold("B", item):
-		return 30 * numberOff, nil
-	case strings.EqualFold("C", item):
-		return 20 * numberOff, nil
-	case strings.EqualFold("D", item):
-		return 15 * numberOff, nil
-	default:
-		return 0, fmt.Errorf("no price for item SKU %s", item)
-	}
-}
-
-type NominalPricer struct {
-}
-
-func (N NominalPricer) ItemPrice(item string, numberOff int) (int, error) {
-	return 1 * numberOff, nil
-}
